24f48f8e7d892c768745120f4a87cb919ee5da8e
Ihor Savenko <csigorek@gmail.com>
2023-11-02 15:26:25 +0000

    added pic option to file

--- api/private/warehouse/__init__.py
+++ api/private/warehouse/__init__.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, File, UploadFile
+from fastapi.responses import FileResponse
 from app.schemas.warehouse import NewLocation, NewStorage, NewContainer, NewItem
 from app.models.warehouse import Location, Storage, Container, Item, PermissionRole
 from app.core.authorization import auth_required
+from os import path as os_path
 import re
 
 warehouse_router = APIRouter(prefix="/warehouse")
@@ -26,11 +28,7 @@ async def create_location(payload: NewLocation, user: dict = Depends(auth_requir
     location = await Location.create(Location(**payload.model_dump()))
 
     await PermissionRole.create(
-        PermissionRole(
-            user_id=user["sub"],
-            location_code=location.code,
-            role="admin"
-        )
+        PermissionRole(user_id=user["sub"], location_code=location.code, role="admin")
     )
 
     return location
@@ -44,10 +42,13 @@ async def get_location(code, user: dict = Depends(auth_required)):
 
     user_role = await PermissionRole.find_one(
         PermissionRole.user_id == user["sub"],
-        PermissionRole.location_code == location.code
+        PermissionRole.location_code == location.code,
     )
     if not user_role or user_role.role not in ["admin", "editor", "viewer"]:
-        raise HTTPException(status_code=403, detail="User does not have permission to view this location")
+        raise HTTPException(
+            status_code=403,
+            detail="User does not have permission to view this location",
+        )
 
     return location
 
@@ -60,25 +61,37 @@ async def export_location(code, user: dict = Depends(auth_required)):
 
     user_role = await PermissionRole.find_one(
         PermissionRole.user_id == user["sub"],
-        PermissionRole.location_code == location.code
+        PermissionRole.location_code == location.code,
     )
 
     if not user_role or user_role.role not in ["admin", "editor", "viewer"]:
-        raise HTTPException(status_code=403, detail="User does not have permission to export this location")
-
+        raise HTTPException(
+            status_code=403,
+            detail="User does not have permission to export this location",
+        )
     loc_exp = {
-        "location": location,
-        "storages": []
+        "location": await Location.find_one(Location.code == code),
+        "storages": [
+            {
+                "storage": storage,
+                "containers": [
+                    {
+                        "container": container,
+                        "items": [
+                            item
+                            async for item in Item.find(
+                                Item.container_code == container.code
+                            )
+                        ],
+                    }
+                    async for container in Container.find(
+                        Container.storage_code == storage.code
+                    )
+                ],
+            }
+            async for storage in Storage.find(Storage.location_code == code)
+        ],
     }
-
-    storages = await Storage.find(Storage.location_code == code)
-    for storage in storages:
-        containers = []
-        containers_query = Container.find(Container.storage_code == storage.code)
-        async for container in containers_query:
-            items = [item async for item in Item.find(Item.container_code == container.code)]
-            containers.append({"container": container, "items": items})
-        loc_exp["storages"].append({"storage": storage, "containers": containers})
 
     return loc_exp
 
@@ -92,16 +105,21 @@ async def create_storage(payload: NewStorage, user: dict = Depends(auth_required
     user_role = await PermissionRole.find_one(
         PermissionRole.user_id == user["sub"],
         PermissionRole.location_code == location.code,
-        PermissionRole.role == "admin"
+        PermissionRole.role == "admin",
     )
     if not user_role:
-        raise HTTPException(status_code=403, detail="User does not have permission to create storage in this location")
+        raise HTTPException(
+            status_code=403,
+            detail="User does not have permission to create storage in this location",
+        )
 
     code = validate_code(payload.code)
     if code is None:
         raise HTTPException(status_code=400, detail="Invalid storage code")
 
-    if await Storage.find_one(Storage.code == code, Storage.location_code == location.code):
+    if await Storage.find_one(
+        Storage.code == code, Storage.location_code == location.code
+    ):
         raise HTTPException(status_code=400, detail="Storage already exists")
 
     storage = await Storage.create(Storage(**payload.model_dump()))
@@ -119,17 +137,22 @@ async def create_container(payload: NewContainer, user: dict = Depends(auth_requ
     user_role = await PermissionRole.find_one(
         PermissionRole.user_id == user["sub"],
         PermissionRole.location_code == location_code,
-        PermissionRole.role in ["admin", "editor"]
+        PermissionRole.role == "admin" or PermissionRole.role == "editor",
     )
 
     if not user_role:
-        raise HTTPException(status_code=403, detail="User doesn't have permission to create container in this storage")
+        raise HTTPException(
+            status_code=403,
+            detail="User doesn't have permission to create container in this storage",
+        )
 
     code = validate_code(payload.code)
     if code is None:
         raise HTTPException(status_code=400, detail="Invalid container code")
 
-    if await Container.find_one(Container.name == payload.name, Container.storage_code == storage.code):
+    if await Container.find_one(
+        Container.name == payload.name, Container.storage_code == storage.code
+    ):
         raise HTTPException(status_code=400, detail="Container already exists")
 
     container = await Container.create(Container(**payload.model_dump()))
@@ -138,22 +161,127 @@ async def create_container(payload: NewContainer, user: dict = Depends(auth_requ
 
 
 @warehouse_router.post("/item")
-async def create_item(payload: NewItem, user: dict = Depends(auth_required)):
+async def create_item(
+    payload: NewItem,
+    user: dict = Depends(auth_required),
+):
     container = await Container.find_one(Container.code == payload.container_code)
     if not container:
         raise HTTPException(status_code=404, detail="Container not found")
 
-    location_code = (await Storage.find_one(Storage.code == container.storage_code)).location_code
+    location_code = (
+        await Storage.find_one(Storage.code == container.storage_code)
+    ).location_code
 
     user_role = await PermissionRole.find_one(
         PermissionRole.user_id == user["sub"],
         PermissionRole.location_code == location_code,
-        PermissionRole.role in ["admin", "editor"]
+        PermissionRole.role == "admin" or PermissionRole.role == "editor",
     )
 
     if not user_role:
-        raise HTTPException(status_code=403, detail="User does not have permission to create an item in this container")
+        raise HTTPException(
+            status_code=403,
+            detail="User does not have permission to create an item in this container",
+        )
 
     item = await Item.create(Item(**payload.model_dump()))
+    return item
+
+
+@warehouse_router.get("/item/{itemid}")
+async def get_item(itemid, user: dict = Depends(auth_required)):
+    item = await Item.get(itemid)
+    if not item:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    container = await Container.find_one(Container.code == item.container_code)
+    if not container:
+        raise HTTPException(status_code=404, detail="Container not found")
+
+    location_code = (
+        await Storage.find_one(Storage.code == container.storage_code)
+    ).location_code
+
+    user_role = await PermissionRole.find_one(
+        PermissionRole.user_id == user["sub"],
+        PermissionRole.location_code == location_code,
+    )
+    if not user_role or user_role.role not in ["admin", "editor", "viewer"]:
+        raise HTTPException(
+            status_code=403,
+            detail="User does not have permission to view this item",
+        )
+
+    item = item.model_dump()
+
+    if os_path.isfile(f"static/{item['id']}.jpg"):
+        item["has_picture"] = True
+    else:
+        item["has_picture"] = False
 
     return item
+
+
+@warehouse_router.get("/item/{itemid}/picture")
+async def get_item_picture(itemid, user: dict = Depends(auth_required)):
+    item = await Item.get(itemid)
+    if not item:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    container = await Container.find_one(Container.code == item.container_code)
+    if not container:
+        raise HTTPException(status_code=404, detail="Container not found")
+
+    location_code = (
+        await Storage.find_one(Storage.code == container.storage_code)
+    ).location_code
+
+    user_role = await PermissionRole.find_one(
+        PermissionRole.user_id == user["sub"],
+        PermissionRole.location_code == location_code,
+    )
+    if not user_role or user_role.role not in ["admin", "editor", "viewer"]:
+        raise HTTPException(
+            status_code=403,
+            detail="User does not have permission to view this item",
+        )
+
+    if os_path.isfile(f"static/{item.id}.jpg"):
+        return FileResponse(f"static/{item.id}.jpg")
+    else:
+        raise HTTPException(status_code=404, detail="Picture not found")
+
+
+@warehouse_router.patch("/item/{itemid}/picture")
+async def set_item_picture(
+    itemid, picture: UploadFile = File(None), user: dict = Depends(auth_required)
+):
+    item = await Item.get(itemid)
+    if not item:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    container = await Container.find_one(Container.code == item.container_code)
+    if not container:
+        raise HTTPException(status_code=404, detail="Container not found")
+
+    location_code = (
+        await Storage.find_one(Storage.code == container.storage_code)
+    ).location_code
+
+    user_role = await PermissionRole.find_one(
+        PermissionRole.user_id == user["sub"],
+        PermissionRole.location_code == location_code,
+        PermissionRole.role == "admin" or PermissionRole.role == "editor",
+    )
+    if not user_role:
+        raise HTTPException(
+            status_code=403,
+            detail="User does not have permission to set the picture of this item",
+        )
+
+    if picture:
+        with open(f"static/{item.id}.jpg", "wb") as buffer:
+            buffer.write(await picture.read())
+
+    return {"message": "Picture set successfully"}
